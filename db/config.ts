436c57589e4b34e7afa24bea03a25021cc9fbf58
kaleb.garner.15@gmail.com <kalebgarner@Macmini.attlocal.net>
2025-01-31 22:33:21 -0600

    new db table addition

--- db/config.ts
+++ db/config.ts
@@ -13,8 +13,23 @@ const Work = defineTable({
   }
 })
 
+const MyWork = defineTable({
+  columns: {
+    url: column.text(),
+    type: column.text(),
+    title: column.text(),
+    headline: column.text(),
+    date: column.text(),
+    thumbnail: column.text(),
+    tldr: column.text(),
+    priority: column.number(),
+    body: column.json()
+  }
+})
+
+
 export default defineDb({
   tables: {
-    Work
+    Work, MyWork
   }
 });
